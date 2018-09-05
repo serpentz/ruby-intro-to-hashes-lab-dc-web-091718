d32b0d53fc2426e9abbcd190656eadb4b42afd68
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-05 15:01:02 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,23 +54,23 @@ def monopoly_with_third_tier
 	      pieces: 4,
 	      names: {
 	        :reading_railroad => {
-	          "mortgage_value" => "$100"
+	           "mortgage_value" => "$100"
 	          },
 	        :pennsylvania_railroad => {
-	          "mortgage_value" => "$200"
+	           "mortgage_value" => "$200"
 	          },
 	          :b_and_o_railroad => {
-	            "mortgage_value" => "$400"
+	           "mortgage_value" => "$400"
 	          },
 	          :shortline => {
-	            "mortgage_value" => "$800"
+	           "mortgage_value" => "$800"
 	          }
 	      },
 	      rent_in_dollars: {
-	        :one_piece_owned => 25,
+	           :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
 	        :three_pieces_owned => 100,
-	        :four_pieces_owned => 200
+	         :four_pieces_owned => 200
 	      }
 	    }
 	  }
